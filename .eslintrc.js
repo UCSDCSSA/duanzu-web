101b955ed2572431d0cee0c30558fc1b30a12a35
bbyiinini <gakang@ucsd.edu>
2018-11-06 16:45:21 -0800

    fix some linter err
    change eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
+  "env": {
+    "browser": true
+  },
   "extends": [
     "airbnb",
     "plugin:flowtype/recommended"
   ],
   "parser": "babel-eslint",
   "settings": {
-    "import/resolver": "babel-plugin-root-import"
+    "import/resolver": {
+      "babel-module": {}
+    }
   },
   "plugins": [
     "flowtype"
